5281cf5913888a79279f71589c47168b6bae6523
designer (Designer) <>
2026-05-20 19:08:52 +0200

    Bootstrap

--- resources/ObjectExternal/DemoCustomDisp/CLASS.js
+++ resources/ObjectExternal/DemoCustomDisp/CLASS.js
@@ -10,41 +10,42 @@ class DemoCustomDisp { // eslint-disable-line no-unused-vars
 	 * @param params Parameters
  	 */
 	static async render(params) {
-		const app = simplicite.session({
-			endpoint: 'ui',
+		const session = simplicite.session({
+			endpoint: simplicite.constants.ENDPOINT_UI,
 			authtoken: params._authtoken, // set in Java
 			ajaxkey: params._ajaxkey // set in Java
 		});
-		app.info(`Lib version: ${simplicite.constants.MODULE_VERSION}`);
+		session.info(`Lib version: ${simplicite.constants.MODULE_VERSION}`);
 
-		const user = await app.getGrant();
+		const user = await session.getGrant();
 		console.log(user);
-		app.info(`User: ${user.firstname} ${user.lastname} (${user.login})`);
+		session.info(`User: ${user.firstname} ${user.lastname} (${user.login})`);
 
-		const prd = app.getBusinessObject('DemoProduct');
-		const list = await prd.search({ demoPrdAvailable: true });
-		const ul = $('<ul/>');
-		for (const item of list) {
-			ul.append($('<li/>').data('item', item)
-				.append($('<img/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)))
-				.append($('<h1/>').text(item.demoPrdName))
-				.append($('<h2/>').text(item.demoPrdReference))
-				.append($('<p/>').html(item.demoPrdDescription))
-			);
+		const prd = session.getBusinessObject('DemoProduct');
+		const products = await prd.search({ demoPrdAvailable: true });
+		const catalog = $('<div class="row row-cols-5"/>');
+		for (const product of products) {
+			catalog.append($('<div class="col"/>')
+				.append($('<div class="m-1 shadow card"/>')
+					.append($('<img class="p-4 card-img-top"/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', product)))
+					.append($('<h5 class="p-1 card-title"/>').text(product.demoPrdName))
+					.append($('<h6 class="p-1 card-subtitle"/>').text(product.demoPrdReference))
+					.append($('<p class="p-2 card-text"/>').html(product.demoPrdDescription))
+			));
 		}
 		
         const scopes = $('<div/>');
-        for (const scope of user.apps) {
-        	const name = scope.home || scope.scope /* v7+ */; 
-            if (name != user.scopeName)
-                scopes.append($('<a/>', { href: `/ui?scope=${name}` }).text(scope.label)).append($('<span/>').text(' | '));
+        for (const app of user.apps) {
+        	const name = app.home; 
+            if (name != user.home)
+                scopes.append($('<a/>', { href: `/ui?scope=${name}` }).text(app.label)).append($('<span/>').text(' | '));
         }
 
 		$('#democustomdisp')
-			.append($('<div/>')
+			.append($('<div class="alert alert-secondary"/>')
 				.append($('<div/>').text(`Hello ${user.firstname} ${user.lastname} (${user.login})`))
 				.append(scopes.append($('<a/>', { href: '/logout' }).text('Logout'))))
 			.append($('<h1/>').text('Product catalog'))
-			.append(ul);
+			.append(catalog);
 	}
 }
